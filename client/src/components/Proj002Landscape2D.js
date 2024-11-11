217801975cdb0368cde297f1c34103162a728163
Michael James <mikeyj777@gmail.com>
2024-11-10 23:22:40 -0500

    2 - working on it

--- client/src/components/Proj002Landscape2D.js
+++ client/src/components/Proj002Landscape2D.js
@@ -4,7 +4,10 @@ class Resource {
   constructor(x, y, minValue = 0, maxValue = 100) {
     this.x = x;
     this.y = y;
-    this.value;
+    this.type = null;
+    this.color = "#FF0000";
+    this.colorRGB = [255, 0, 0];
+    this.value = -1;
     this.minValue = minValue;
     this.maxValue = maxValue;
   }
@@ -14,56 +17,177 @@ class Resource {
   }
 }
 
-class A extends Resource { //Aethernium 
+class A extends Resource { //Aethernium
   constructor(x, y, maxValue = 100, minValue = 30) {
-    super(x, y, value);
+    super(x, y, minValue, maxValue);
+    this.type = "A";
+    this.colorRGB = [255, 0, 0];
     this.color = "#FF0000";
     this.getValue()
   }
 }
 
-class Z extends Resource { //Zephyrix
-  constructor(x, y, maxValue = 200, minValue = 50) {
-    super(x, y, value); 
+class B extends Resource { //Blazilite
+  constructor(x, y, maxValue = 100, minValue = 30) {
+    super(x, y, minValue, maxValue);
+    this.type = "B";
+    this.colorRGB = [0, 255, 0];
+    this.color = "#00FF00";
+    this.getValue()
+  }
+}
+
+class C extends Resource { //Chronite
+  constructor(x, y, maxValue = 100, minValue = 30) {
+    super(x, y, value);
+    this.type = "C";
+    this.colorRGB = [0, 0, 255];
     this.color = "#0000FF";
     this.getValue()
   }
 }
 
-class M extends Resource { //Magnophorium 
-  constructor(x, y, aAmount, zAmount, value = 30) {
-    super(x, y, value); 
-    this.color = "#00FF00";
-    this.value = value; // this will be updated based on the ratio of A to Z used by the agent.
-    this.aAmount = aAmount;
-    this.zAmount = zAmount;
+class ResourceCollection {
+  constructor(type = "A"){
+    this.type = type;
+    this.collection = [];
   }
 
-  calculateMagnophorium(optimalRatio, maxBoostPercent){
-    // Ensure ratio is between 0 and 1
-    const ratio = Math.max(0, Math.min(1, ratioA));
-    
-    // Calculate the weighted average at this ratio
-    const weightedAvg = valueA * ratio + valueZ * (1 - ratio); // find some mechanism to calculate values of A and Z.  perhaps the weighted average of the components of each.
-    
-    // Build quadratic that peaks at optimal ratio with maxBoost
-    // and returns to weighted average at pure A (ratio=1) or pure Z (ratio=0)
-    const a = -maxBoostPercent / (optimalRatio * optimalRatio - optimalRatio);
-    const b = 2 * maxBoostPercent * optimalRatio / (optimalRatio * optimalRatio - optimalRatio);
-    const boost = a * ratio * ratio + b * ratio;
-    
-    this.value =  weightedAvg * (1 + boost);
-  };
+  getValue() {
+    let value = 0;
+    this.collection.forEach((resource) => {
+      value += resource.value;
+    });
+    return value;
+  }
+}
+
+class Mix {
+  constructor(resourceCollection1, resourceCollection2, optimalRatio = 0.33, maxBoostPercent = 0.5) {
+    this.type = "Mix";
+    this.resourceCollection1 = resourceCollection1;
+    this.resourceCollection2 = resourceCollection2;
+    this.optimalRatio = optimalRatio;
+    this.maxBoostPercent = maxBoostPercent;
+    this.getType();
+    this.getValue();
+  }
+
+  getType() {
+    if (!this.resourceCollection1 || !this.resourceCollection2) return;
+    this.type = this.collectionResource1.type + this.collectionResouce2.type;
+  }
+
+  getValue() {
+    // optimal ratio is the most valuable combination of the two resources.  
+    // at this point, the value is boosted above the max of either resource value
+    // this method uses the total value of the two resource collections 
+    if (!this.resourceCollection1 || !this.collectionResouce2) return;
+    let value1 = 0;
+    this.collectionResource1.collection.forEach((resource) => {
+      value1 += resource.value;
+    });
+    let value2 = 0;
+    this.collectionResouce2.collection.forEach((resource) => {
+      value2 += resource.value;
+    });
+    let totValue = value1 + value2;
+    let ratio = value1 / totValue;
+    let boosted = this.maxBoostPercent * Math.max(value1, value2);
+    const m1 = (boosted - value1) / (this.optimalRatio);
+    const b1 = boosted - m * this.optimalRatio;
+    const m2 = (value2 - boosted) / (1 - this.optimalRatio);
+    const b2 = value2 - m2 * (1 - this.optimalRatio);
+    if (ratio <= this.optimalRatio) {
+      this.value = m1 * ratio + b1;
+    } else {
+      this.value = m2 * ratio + b2;
+    }
+  }
+}
+
+const resourceDict = {
+  A: A,
+  B: B,
+  C: C,
+};
+
+class Grid {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.grid = [];
+    for (let i = 0; i < width; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < height; j++) {
+        this.grid[i][j] = null;
+      }
+    }
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.resourceCollections = {
+      A: new ResourceCollection("A"),
+      B: new ResourceCollection("B"),
+      C: new ResourceCollection("C"),
+    };
+    this.compounds = [];
+    this.money = 0;
+  }
+
+  
 
 }
 
+const Proj002Landscape2D = ({optimalRatio = 0.33, maxBoostPercent = 0.5, agentMaxMoney = 1000, gridSide = 30, numAgents = 300, maxResourceCount = 300}) => {
 
-const Proj002Landscape2D = ({optimalRatio = 0.33, maxBoostPercent = 0.5}) => {
-
-  // optimalRatio is between 0 and 1, representing the A:Z ratio where max boost occurs
+  // optimalRatio is between 0 and 1, representing the resource mix ratio where max boost occurs
+  // ratio is interms of A component to other component (current AB and AC are possible compounds)
   // maxBoostPercent is the percentage boost at the optimal point (e.g. 0.25 for 25% boost)
-  // valueA and valueZ are the base values of the pure elements
+  const grid = new Grid(gridSide, gridSide);
+
+  const getResource = () => {
+    let resourceArray  = Object.keys(resourceDict);
+    let randomNumber = Math.random();
+    let resourceIndex  = Math.floor(randomNumber * resourceArray.length);
+    let randomKey    = resourceArray[resourceIndex];
+    let randomValue  = resourceDict[randomKey];
+    return randomValue;
+  }
   
+  const poulateGrid = () => {
+    for (let i = 0; i < numAgents; i++) { 
+      let agentHere = true;
+      while (agentHere) {
+        const x = Math.floor(Math.random() * gridSide);
+        const y = Math.floor(Math.random() * gridSide); 
+        // only one agent per cell
+        if (grid.grid[x][y] instanceof Agent) continue;
+        const agent = new Agent(x, y);
+        grid.grid[x][y] = agent;  
+        agentHere = false;
+      }
+    }
+
+    for (let i = 0; i < maxResourceCount; i++) {
+      const x = Math.floor(Math.random() * gridSide);
+      const y = Math.floor(Math.random() * gridSide);
+      // resources can exist anywhere regardless of current occupancy in cell.
+      const resource = getResource(x, y);
+    }
+
+    return newGrid;
+  }
+
+
+  useEffect(() => {
+    const newGrid = populateGrid();
+    setGrid(newGrid);
+  }, [gridSide, numAgents]);
   
 
   return ( 
